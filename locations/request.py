bbfe9d5422e20e7c4a954d3bf00364dccea5ddcb
Caleb Wagner <calebwagner5@gmail.com>
2021-07-12 13:35:29 -0500

    updated locations modules to display list

--- locations/request.py
+++ locations/request.py
@@ -1,3 +1,20 @@
+LOCATIONS = [
+    {
+      "id": 1,
+      "name": "Nashville North",
+      "address": "8445 Johnson Pike"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South",
+      "address": "210 Emory Drive"
+    }
+]
+
+def get_all_locations():
+    return LOCATIONS
+
+
 # Function with a single parameter
 def get_single_location(id):
     # Variable to hold the found animal, if it exists
