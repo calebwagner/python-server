e4c4d466a2711d36669356f7319726643316aa4c
Caleb Wagner <calebwagner5@gmail.com>
2021-07-13 13:22:24 -0500

    exported create customer function and imported it to main module

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
-from customers.request import get_all_customers, get_single_customer
+from customers.request import get_all_customers, get_single_customer, create_customer
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from locations.request import get_all_locations, get_single_location, create_location
 from animals.request import get_all_animals, get_single_animal, create_animal
-from employees.request import get_all_employees, get_single_employee
+from employees.request import get_all_employees, get_single_employee, create_employee
 import json
 
 
@@ -118,7 +118,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Initialize new animal
         new_animal = None
         new_location = None
-
+        new_employee = None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
@@ -132,6 +132,11 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_location = create_location(post_body)
         # Encode the new animal and send in response
         self.wfile.write(f"{new_location}".encode())
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+        # Encode the new employee and send in response
+        self.wfile.write(f"{new_employee}".encode())
 
 
     # Here's a method on the class that overrides the parent's method.
