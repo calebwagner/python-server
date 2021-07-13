d4ee16c8e2cf89947edad304b7d7a6cd3b068a6f
Caleb Wagner <calebwagner5@gmail.com>
2021-07-13 13:42:55 -0500

    added delete method for animals list

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from customers.request import get_all_customers, get_single_customer, create_customer
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from locations.request import get_all_locations, get_single_location, create_location
-from animals.request import get_all_animals, get_single_animal, create_animal
+from animals.request import get_all_animals, get_single_animal, create_animal, delete_animal
 from employees.request import get_all_employees, get_single_employee, create_employee
 import json
 
@@ -143,6 +143,21 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_customer = create_customer(post_body)
         # Encode the new customer and send in response
         self.wfile.write(f"{new_customer}".encode())
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
 
 
     # Here's a method on the class that overrides the parent's method.
